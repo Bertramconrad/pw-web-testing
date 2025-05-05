5c449ba7e01c47f9f671674496f375a3e046ec8f
Daniel Riveros <danielriverosdigital@gmail.com>
2025-05-05 10:22:55 -0400

    Working with Checkboxes and Methods

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -45,3 +45,50 @@ test.describe('Form Layouts Page', ()=> {
     })
 
 })
+
+test('Checkboxes', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    //Forcing the click on a checkbox when its visibility is hidden
+    await page.getByRole('checkbox', {name: "Hide on click"}).click({force: true, delay: 500})
+    await page.waitForTimeout(1000)
+    await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+    await page.waitForTimeout(1000)
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).click({force: true, delay: 500})
+
+    //await page.getByRole('checkbox', {name: "Show toast with icon"}).click({force: true, delay: 500})
+    
+    const radioButtonHideOnClick = await page.getByRole('checkbox', {name: "Hide on click"}).isChecked()
+    const radioButtonPrevent = await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).isChecked()
+    const radioButtonShow = await page.getByRole('checkbox', {name: "Show toast with icon"}).isChecked()
+
+    //console.log(radioButtonHideOnClick)
+    //console.log(radioButtonPrevent)
+    //console.log(radioButtonShow)
+
+    if (!radioButtonHideOnClick) {
+        await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+    } else await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+
+    if (!radioButtonPrevent) {
+        await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+    } else await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).uncheck({force: true})
+
+    if (!radioButtonShow) {
+        await page.getByRole('checkbox', {name: "Show toast with icon"}).check({force: true})
+    } else await page.getByRole('checkbox', {name: "Show toast with icon"}).uncheck({force: true})
+
+    //Array from the checkboxes and assertion to verify the checked status for each one.
+    await page.waitForTimeout(3000)
+    const allBoxes = page.getByRole('checkbox')
+    for (const box of await allBoxes.all()){
+         //await box.check({force: true})         
+         //expect(await box.isChecked()).toBeTruthy()
+         await box.uncheck({force: true})
+         expect(await box.isChecked()).toBeFalsy()
+    }
+
+})
