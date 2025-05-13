bf45f29204e8b7d29cf1f68de8a7518dd57e6ceb
Daniel Riveros <danielriverosdigital@gmail.com>
2025-05-13 09:45:46 -0400

    Working On Docker Container and Docker Compose

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,10 @@ require('dotenv').config();
 export default defineConfig<TestOptions>({
 //export default defineConfig({
   timeout: 50000,
-  globalTimeout: 60000,
+
+  //The next intrucción was commented for implementation of Docker.
+  //globalTimeout: 60000,
+
   expect: {
     timeout: 50000,
     toMatchSnapshot: {maxDiffPixels: 50}
@@ -131,9 +134,9 @@ export default defineConfig<TestOptions>({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+     command: 'npm run start',
+     url: 'http://localhost:4200/',
+     //reuseExistingServer: !process.env.CI,
+  },
 });
